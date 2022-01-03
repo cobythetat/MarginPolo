19fe053a431362d6ae6e63b13e0e866910712cd3
cobythetat <loycev@protonmail.com>
2022-01-03 18:15:13 +0100

    working bot class

--- bot.py
+++ bot.py
@@ -1,8 +1,139 @@
 from poloniex import Poloniex, PoloniexError
-from helpers import read_keys_from_file, calculate_avg_true_range
+from helpers import read_keys_from_file, calculate_avg_true_range, dump_json_to_file
+from constants import *
+import time
 
 
 class Bot:
     def __init__(self):
         api_key, api_secret = read_keys_from_file()
-        self.client = Poloniex(api_key, secret)
+        self.client = Poloniex(api_key, api_secret)
+        self.stop_losses = {}
+        self.stop_distances = {}
+        self.ticks = {}
+        self.stamps = {}
+        self.atr = {}
+
+    def log_finished_trade(self, pair, position, close):
+        position['stamp'] = self.stamps[pair]
+        position['pair'] = pair
+        position['atr'] = self.atr[pair]
+        entry = float(position['basePrice'])
+        exit = float(closer['resultingTrades'][0]['rate'])
+        pl = (max([entry, exit]) - min([entry, exit])) * float(position['amount'])
+        pl_percent = (max([entry, exit]) - min([entry, exit])) / exit
+        data = {
+            "position": position,
+            "closing_trades": close['resultingTrades'],
+            "stop_losses": self.stop_losses[pair],
+            "tickers": self.ticks[pair],
+            "profit_loss": [pl, pl_percent]
+        }
+        path = 'trades/%s-%s-%s' % (pair, position['direction'], str(position['stamp']))
+        dump_json_to_file(path, data)
+        del self.stop_distances[pair]
+        del self.stop_losses[pair]
+        del self.ticks[pair]
+        del self.atr[pair]
+
+    def get_tick(self, pair, tickers):
+        tick = tickers[pair]
+        bid = float(tick['highestBid'])
+        ask = float(tick['lowestAsk'])
+        if pair not in self.ticks:
+            self.ticks[pair] = []
+        self.ticks[pair].append({"bid": bid, "ask": ask})
+        return bid, ask
+
+    def set_stop_loss(self, pair, base_price, direction):
+        start = time.time() - (CANDLE_PERIOD * ATR_PERIOD)
+        end = time.time()
+        candle_chart = self.client.returnChartData(pair, CANDLE_PERIOD, start, end)
+        atr = calculate_avg_true_range(candle_chart)
+        distance = atr * STOP_LOSS
+        if direction == 'short':
+            stop_loss = base_price + distance
+            self.stop_losses[pair] = [stop_loss]
+        else:
+            stop_loss = base_price - distance
+        self.stop_losses[pair] = [stop_loss]
+        self.stop_distances[pair] = distance
+        self.stamps[pair] = end
+        self.atr[pair] = atr
+
+    def get_open_positions(self):
+        open_positions = {}
+        position_data = self.client.getMarginPosition('all')
+        for p in position_data:
+            if position_data[p]['type'] not in ['short', 'long']:
+                continue
+            open_positions[p] = position_data[p]
+        return open_positions
+
+    def run(self):
+        while True:
+            try:
+                open_positions = self.get_open_positions()
+                time.sleep(1)
+                if not open_positions:
+                    time.sleep(TICK_RATE / 2)
+                    continue
+                tickers = self.client.returnTicker()
+                for pair in open_positions:
+                    position = open_positions[pair]
+                    direction = position['type']
+                    quote, base = pair.split('_')
+                    base_price = float(position['basePrice'])
+                    amount = float(position['amount'])
+                    bid, ask = self.get_tick(pair, tickers)
+
+                    if pair not in self.stop_distances:
+                        self.set_stop_loss(pair, base_price, direction)
+                        print('')
+
+                    if direction == 'short':
+                        pl = (base_price - ask) * abs(amount)
+                        pl_percent = (base_price - ask) / ask
+
+                        if ask < base_price:
+                            accelerate = 1 - (abs(pl_percent) * ACCELERATE)
+                            self.stop_distances[pair] *= accelerate
+                            new_stop_loss = ask + self.stop_distances[pair]
+                            if new_stop_loss < self.stop_losses[pair][-1]:
+                                self.stop_losses[pair].append(new_stop_loss)
+
+                        stop_loss = self.stop_losses[pair][-1]
+                        distance = self.stop_distances[pair]
+
+                        if ask < (stop_loss - distance):
+                            new_stop_loss = ask + distance
+                            self.stop_losses[pair].append(new_stop_loss)
+
+                        if ask > self.stop_losses[pair][-1]:
+                            # stop loss reached
+                            close_position = self.client.closeMarginPosition(pair)
+                            self.log_finished_trade(pair, position, close_position)
+
+                    if direction == 'long':
+                        pl = (bid - base_price) * abs(amount)
+                        pl_percent = (bid - base_price) / bid
+
+                        if bid > base_price:
+                            accelerate = 1 - (abs(pl_percent) * ACCELERATE)
+                            self.stop_distances[pair] *= accelerate
+                            new_stop_loss = bid - self.stop_distances[pair]
+                            if new_stop_loss > self.stop_losses[pair][-1]:
+                                self.stop_losses[pair].append(new_stop_loss)
+
+                        if bid > (self.stop_losses[pair][-1] + self.stop_distances[pair]):
+                            new_stop_loss = bid - self.stop_distances[pair][-1]
+                            self.stop_losses[pair].append(new_stop_loss)
+
+                        if bid < self.stop_losses[pair][-1]:
+                            # stop loss reached
+                            close_position = self.client.closeMarginPosition(pair)
+                            self.log_finished_trade(pair, position, close_position)
+                time.sleep(TICK_RATE)
+            except KeyboardInterrupt:
+                print('manual interrupt')
+                return
