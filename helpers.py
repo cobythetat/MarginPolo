02d074d9572e7767514e756eb8e18c6bb2543152
cobythetat <loycev@protonmail.com>
2022-01-03 13:59:37 +0100

    added helper functions

---
+++ helpers.py
@@ -0,0 +1,22 @@
+
+def read_keys_from_file():
+    with open('api_credentials.txt') as file:
+        api_key = file.readline().strip()
+        api_secret = file.readline().strip()
+        return api_key, api_secret
+
+
+def calculate_avg_true_range(candle_chart, period=14):
+    true_ranges = []
+    previous_candle = None
+    candles = candle_chart[-abs(period):]
+    for i in range(len(candles)):
+        if i == 0:
+            previous_candle = candles[i]
+        x = float(candles[i]['high']) - float(candles[i]['low'])
+        y = abs(float(candles[i]['high']) - float(previous_candle['close']))
+        z = abs(float(candles[i]['low']) - float(previous_candle['close']))
+        true_ranges.append(max([x, y, z]))
+        previous_candle = candles[i]
+        i += 1
+    return sum(true_ranges) / len(true_ranges)
