9d21721f348ffd3fdbe7b0927df02b8f9404973f
cobythetat <loycev@protonmail.com>
2022-01-08 14:07:08 +0100

    tweak path

--- constants.py
+++ constants.py
@@ -8,4 +8,4 @@ CANDLE_PERIOD = 1800  # timeframe for chart. For valid inputs, see Poloniex Docu
 ATR_PERIOD = 14  # last n candles to use for calculating the ATR value
 STOP_LOSS = 2  # ATR is multiplied by this number, and subtracted/added to base price to give us stop loss
 ACCELERATE = 1  # +% profits are subtracted from the stop loss distance after being multiplied by this number
-TRADES_FOLDER_PATH = "trades/"  # folder for saving .json files with trade data
+TRADES_FOLDER_PATH = 'trades'  # folder for saving .json trade data
